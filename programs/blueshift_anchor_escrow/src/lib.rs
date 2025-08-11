b2f559e2822cabf20520b2e1c638295c6906926c
babyJesus12 <babyjesus7695@gmail.com>
2025-08-11 20:35:40 +0530

    Escrow Completed

--- programs/blueshift_anchor_escrow/src/lib.rs
+++ programs/blueshift_anchor_escrow/src/lib.rs
@@ -1,28 +1,27 @@
 use anchor_lang::prelude::*;
- 
 mod state;
 mod errors;
-mod instructions;
-use instructions::*;
- 
+pub mod instructions;
+pub use instructions::*;
+
 declare_id!("22222222222222222222222222222222222222222222");
- 
+
 #[program]
 pub mod blueshift_anchor_escrow {
     use super::*;
- 
+
     #[instruction(discriminator = 0)]
     pub fn make(ctx: Context<Make>, seed: u64, receive: u64, amount: u64) -> Result<()> {
-        //...
+        instructions::make::handler(ctx, seed, receive, amount)
     }
- 
+
     #[instruction(discriminator = 1)]
     pub fn take(ctx: Context<Take>) -> Result<()> {
-        //...
+        ctx.accounts.take()
     }
- 
+    
     #[instruction(discriminator = 2)]
     pub fn refund(ctx: Context<Refund>) -> Result<()> {
-        //...
+        ctx.accounts.refund_to_maker_and_close_account()
     }
 }
